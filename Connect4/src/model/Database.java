6a3dba97b087c4e9f16f4995a70d775cda21384d
Brian Delhaisse <bdelhais@ulb.ac.be>
2014-04-29 12:46:59 +0200

    Removal of one import in Database.java +
    update of the connect4 panel.

--- Connect4/src/model/Database.java
+++ Connect4/src/model/Database.java
@@ -1,12 +1,9 @@
 package model;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -15,7 +12,6 @@ import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.nio.charset.Charset;
-import java.nio.file.Path;
 
 public class Database {
 	private int maxStates = 2;
--- Connect4/src/view/Connect4Panel.java
+++ Connect4/src/view/Connect4Panel.java
@@ -18,10 +18,21 @@ public class Connect4Panel extends JPanel {
 	
 	private int nRows=6;
 	private int nColumns=7;
+	private int table[][] = new int[nRows][nColumns];
 
 	public Connect4Panel() {
 		this.setMinimumSize(new Dimension(width,height));
 		this.setPreferredSize(new Dimension(width,height));
+		init();
+	}
+	
+	private void init() {
+		for(int i=0; i<nRows; i++)
+			for(int j=0; j<nColumns; j++)
+				table[i][j]=0;
+		//TODOÊremove the 2 following lines.
+		table[5][6]=1;
+		table[5][0]=2;
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -31,19 +42,25 @@ public class Connect4Panel extends JPanel {
 		g2.setColor(Color.BLUE);
 		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
 		
-		//Grid with the empty holes
-		g2.setColor(new Color(237,237,237));
+		//Grid
 		for(int i=0; i<nRows; i++) {
 			for(int j=0; j<nColumns; j++) { 
-				g2.fillOval(this.getWidth()/nColumns*j, this.getHeight()/nRows*i, this.getWidth()/nColumns, this.getHeight()/nRows);
+				if(table[i][j]==0) {
+					//Empty holes
+					g2.setColor(new Color(237,237,237));
+					g2.fillOval(this.getWidth()/nColumns*j, this.getHeight()/nRows*i, this.getWidth()/nColumns, this.getHeight()/nRows);
+				} else if(table[i][j]==1) {
+					//Yellow discs
+					g2.setColor(Color.YELLOW);
+					g2.fillOval(this.getWidth()/nColumns*j, this.getHeight()/nRows*i, this.getWidth()/nColumns, this.getHeight()/nRows);
+				} else {
+					//Red discs
+					g2.setColor(Color.RED);
+					g2.fillOval(this.getWidth()/nColumns*j, this.getHeight()/nRows*i, this.getWidth()/nColumns, this.getHeight()/nRows);	
+				}
 			}
 		}
 		
-		//Yellow discs
-		g2.setColor(Color.YELLOW);
-		
-		//Red discs
-		g2.setColor(Color.RED);
 	}
 
 }
